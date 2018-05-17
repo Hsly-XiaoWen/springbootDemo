a73a8864335c8f3e2bb2192c3b87db3c124965a8
xiaowen <xiaowen@tuandai.com>
2018-05-17 09:21:34 +0800

    系统文件上传自定义配置提交

---
+++ src/main/java/com/tuandai/config/MultipartConfig.java
@@ -0,0 +1,23 @@
+package com.tuandai.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * Created by 肖文 on 2018/5/16
+ * 可以通过MultipartConfig配置类对文件上传进行全局控制。
+ */
+@Configuration
+public class MultipartConfig {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("500MB");
+        factory.setMaxRequestSize("500MB");
+        return factory.createMultipartConfig();
+    }
+}
