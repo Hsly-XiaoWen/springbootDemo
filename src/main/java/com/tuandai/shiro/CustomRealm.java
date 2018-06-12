7e16981c5563b4d00ec7419b662fbafcec79712b
xiaowen <xiaowen@tuandai.com>
2018-06-12 09:56:37 +0800

    shiro自定义Realm实现校验、角色、权限

--- src/main/java/com/tuandai/shiro/CustomRealm.java
+++ src/main/java/com/tuandai/shiro/CustomRealm.java
@@ -3,20 +3,54 @@ package com.tuandai.shiro;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by 肖文 on 2018/6/12
+ * 自定义Realm实现验证
  */
 public class CustomRealm extends AuthorizingRealm {
 
+    HashMap<String,String> maps=new HashMap<>();
+    {
+        maps.put("xiaowen", "123456");
+        super.setName("test");
+    }
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-        return null;
+        String userName= (String) principalCollection.getPrimaryPrincipal();
+        Set<String> roles = getRolesByName(userName);
+        Set<String> permissions = getPermissionsByName(userName);
+        SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+        simpleAuthorizationInfo.setRoles(roles);
+        simpleAuthorizationInfo.setStringPermissions(permissions);
+        return simpleAuthorizationInfo;
     }
 
+    //数据库获取权限信息
+    private Set<String> getPermissionsByName(String userName) {
+        Set<String> permissions = new HashSet<>();
+        permissions.add("delete");
+        permissions.add("update");
+        permissions.add("insert");
+        return permissions;
+    }
+
+    //数据库获取角色信息
+    private Set<String> getRolesByName(String userName) {
+        Set<String> roles = new HashSet<>();
+        roles.add("admin");
+        roles.add("user");
+        return roles;
+    }
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
 
@@ -27,12 +61,13 @@ public class CustomRealm extends AuthorizingRealm {
         if (password == null) {
             return null;
         }
-//        SimpleAuthenticationInfo authenticationInfo=new SimpleAuthenticationInfo("")
-        return null;
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo("xiaowen"
+                ,password,"test");
+        return authenticationInfo;
     }
 
     //从数据库获取凭证
     private String getPasswordByUserName(String username) {
-        return "";
+        return "123456";
     }
 }
--- src/main/java/com/tuandai/shiro/test/CustomRealmTest.java
+++ src/main/java/com/tuandai/shiro/test/CustomRealmTest.java
@@ -1,7 +1,32 @@
 package com.tuandai.shiro.test;
 
+import com.tuandai.shiro.CustomRealm;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
 /**
  * Created by 肖文 on 2018/6/12
  */
 public class CustomRealmTest {
+
+    private CustomRealm customRealm = new CustomRealm();
+
+    @Test
+    public void testAuthentication() {
+        DefaultSecurityManager securityManager = new DefaultSecurityManager();
+        securityManager.setRealm(customRealm);
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+        AuthenticationToken token = new UsernamePasswordToken("xiaowen", "123456");
+        subject.login(token);
+        System.out.println("用户是否已经验证：" + subject.isAuthenticated());
+        System.out.println("用户是否已经验证：" + subject.hasRole("admin"));
+        System.out.println("用户是否已经验证：" + subject.hasRole("user"));
+        System.out.println("用户是否已经验证：" + subject.isPermitted("delete"));
+    }
 }
